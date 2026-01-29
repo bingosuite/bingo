0e6354a15c1a5eab763408b56d5728d899065ad1
Sacha Arseneault <sacha.arseneault@gmail.com>
2026-01-29 16:27:51 -0500

    fix: move target to internal/debugger

--- internal/debugger/target.go
+++ internal/debugger/target.go
@@ -1,18 +1,16 @@
-package main
+package debugger
 
 import (
 	"fmt"
 	"time"
 )
 
-func main() {
-	//fmt.Println("Hello, World")
+func RunTarget() {
 	var count int
 	for {
 		fmt.Println("Hello, World")
 		count = count + 1
 		count = count * 1
 		time.Sleep(2 * time.Second)
-
 	}
 }
