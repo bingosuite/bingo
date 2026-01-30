f91d0d6488b09beb07ab65f3c4fcd56539993ffe
Xavier Lermusieaux <xavier.lermusieaux@gmail.com>
2026-01-30 16:45:42 +0000

    fix: linting errors

--- internal/ws/client.go
+++ internal/ws/client.go
@@ -7,47 +7,49 @@ import (
 )
 
 type Client struct {
-    conn *websocket.Conn
-    hub  *Hub
-    send chan Message
+	conn *websocket.Conn
+	hub  *Hub
+	send chan Message
 }
 
 func NewClient(conn *websocket.Conn, hub *Hub) *Client {
-    return &Client{
-        conn: conn,
-        hub:  hub,
-        send: make(chan Message, 256),
-    }
+	return &Client{
+		conn: conn,
+		hub:  hub,
+		send: make(chan Message, 256),
+	}
 }
 
 func (c *Client) ReadPump() {
-    defer func() {
-        c.hub.Unregister(c)
-        c.conn.Close()
-    }()
-    
-    for {
-        var msg Message
-        err := c.conn.ReadJSON(&msg)
-        if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseNormalClosure) {
-            log.Printf("Client unexpected close: %v", err)
-            break
-        }
-        if err != nil {
-            break
-        }
-        c.hub.SendCommand(msg)
-    }
+	defer func() {
+		c.hub.Unregister(c)
+		c.conn.Close()
+	}()
+
+	for {
+		var msg Message
+		err := c.conn.ReadJSON(&msg)
+		if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseNormalClosure) {
+			log.Printf("Client unexpected close: %v", err)
+			break
+		}
+		if err != nil {
+			break
+		}
+		c.hub.SendCommand(msg)
+	}
 }
 
 func (c *Client) WritePump() {
-    defer c.conn.Close()
+	defer c.conn.Close()
 
-    for message := range c.send {
-        if err := c.conn.WriteJSON(message); err != nil {
-            log.Printf("Client write error: %v", err)
-            return
-        }
-    }
-    c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+	for message := range c.send {
+		if err := c.conn.WriteJSON(message); err != nil {
+			log.Printf("Client write error: %v", err)
+			return
+		}
+	}
+	if err := c.conn.WriteMessage(websocket.CloseMessage, []byte{}); err != nil {
+		panic(err)
+	}
 }
