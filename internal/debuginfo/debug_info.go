095ac387f60d6a596cf364fc1ff9a190038ef48f
Xavier Lermusieaux <xavier.lermusieaux@gmail.com>
2026-03-04 20:18:09 +0000

    docs: comment the debug_info.go interface

--- internal/debuginfo/debug_info.go
+++ internal/debuginfo/debug_info.go
@@ -4,6 +4,7 @@ import (
 	"debug/gosym"
 )
 
+// Target represents information about the debugging target process
 type Target struct {
 	Path string
 	PID  int
@@ -11,8 +12,15 @@ type Target struct {
 }
 
 type DebugInfo interface {
+	// GetTarget returns the Target struct of the debugger instance
 	GetTarget() Target
+
+	// PCToLine returns the filename, line number and function from the stack memory address
 	PCToLine(pc uint64) (file string, line int, fn *gosym.Func)
+
+	// LineToPC returns the memory address, function or an error from the filename and line number
 	LineToPC(file string, line int) (pc uint64, fn *gosym.Func, err error)
+
+	// LookupFunc returns the Func struct who's name corresponds to the value of fn
 	LookupFunc(fn string) *gosym.Func
 }
